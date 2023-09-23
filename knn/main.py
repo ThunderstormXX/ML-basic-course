ac02ff80e206e3a1e8d3baed405c21a1ced5401f
Igoreshka <ignashin.in@phystech.edu>
2023-09-23 21:39:15 +0300

    Сделал домашку по knn , вроде рисует неплохо на 2 признаках

---
+++ knn/main.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_wine
+from sklearn.neighbors import KNeighborsClassifier
+
+from knn import KNearestNeighbor
+wine = load_wine()
+X = wine.data[:, :2]  # Возьмем только первые два признака для простоты
+y = wine.target
+
+# X_train = X[:150]
+# y_train = y[:150]
+# X_test = X[151:]
+# y_test =y[151:]
+
+model = KNearestNeighbor()
+model.fit(X,y)
+
+# y_preds = model.predict(X_test, k=5)
+# print(y_test)
+# print(y_preds)
+
+data = X
+target = y
+
+x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
+y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.2),
+                     np.arange(y_min, y_max, 0.2))
+
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()] , k = 5 ,num_loops=0 )
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.Paired)
+
+# Отобразим точки на графике
+for class_label in set(y):
+    plt.scatter(X[y == class_label][:, 0], X[y == class_label][:, 1], label=f'Class {class_label}')
+
+# Добавим подписи и легенду
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend(title='Classes')
+
+# Отобразим график
+plt.show()
